29953b6ddd9e777dd8467cba06dc14b2ec355394
Pasindu1011 <pasinduraanjana@gmail.com>
2023-12-03 12:37:54 +0530

    background image added

--- index.php
+++ index.php
@@ -5,11 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="resource/css">
     <title>BookShop</title>
     <link rel="icon" href="resource/logo.png">
 </head>
-<body>
-    
+<body class ="main-body">
+<div class="container-fluid vh-100 d-flex justifly-content-center">
+    <div class="row align-content-center">
+    <!--header -->
+    <div class="col-12">
+        <div class="row"></div>
+    </div>
+
+    <!-- header-->
+
+    </div>
+</div>
     
 
     <script src="script.js"></script>
