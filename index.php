31a8350b5c7f1f29cdc0526660789c0ac5d48995
Pasindu1011 <pasinduraanjana@gmail.com>
2023-12-03 10:15:06 +0530

    renamed to logo.png

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>BookShop</title>
+    <link rel="icon" href="resource/logo.png">
 </head>
 <body>
-    <h1>hi world</h1>
+    
     
 
     <script src="script.js"></script>
---
+++ readme.txt
@@ -0,0 +1,1 @@
+MySQL password = tryhard12
