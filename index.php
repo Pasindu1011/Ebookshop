aebfe901a0f8559337baac6c6680881b3f61d5fa
Pasindu1011 <pasinduraanjana@gmail.com>
2023-12-05 10:36:45 +0530

    oclick added

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="resource/css">
     <title>BookShop</title>
     <link rel="icon" href="resource/logo.png">
+ 
 </head>
 <body class ="main-body">
 <div class="container-fluid vh-100 d-flex justify-content-center">
@@ -29,7 +30,7 @@
     <div class="col-12 p-3">
         <div class="row">
             <div class="col-6 d-none d-lg-block background"></div>
-            <div class="col-12 col-lg-6 " id="signupBox">
+            <div class="col-12 col-lg-6 " id="signUpBox">
                 <div class="row g-2">
                     <div class="col-12">
                         <p class="title2">Create New Account</p>
@@ -66,12 +67,12 @@
                         <button class="btn btn-primary">Sign Up</button>
                     </div>
                     <div class="col-12 col-lg-6 d-grid">
-                        <button class="btn btn-dark" onclick="changeView();">Already have an account?Sign In</button>
+                        <button class="btn btn-dark" onclick="ChangeView();">Already have an account?Sign In</button>
                     </div>
                 </div>
 
             </div>
-            <div class="col-12 col-lg-6 d-none"id="signIngBox">
+            <div class="col-12 col-lg-6 d-none"id="signInBox">
                 <div class="row g-2">
                     <div class="col-12">
                         <p class="title2">Sign In</p>
@@ -97,7 +98,7 @@
                         <button class="btn btn-success">Sign In</button>
                     </div>
                     <div class="col-12 col-lg-6 d-grid">
-                        <button class="btn btn-danger">New to Eshop? join Now</button>
+                        <button class="btn btn-danger" onclick="ChangeView();">New to Eshop? join Now</button>
                     </div>
                 </div>
             </div>
@@ -115,8 +116,7 @@
     </div>
 </div>
     
-
-    <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,8 @@
+function ChangeView(){
+
+    var signUpBox = document.getElementById("signUpBox");
+    var signInBox = document.getElementById("signInBox");
+
+    signUpBox.classList.toggle("d-none");
+    signInBox.classList.toggle("d-none");
+}
